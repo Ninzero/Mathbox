935a85857f25039be8da210944cf08e55d1a0f81
Ninzero <Ninzero@users.noreply.github.com>
2016-08-02 23:08:54 +0800

    Add files via upload

--- find prime number.py
+++ find prime number.py
@@ -1,12 +1,14 @@
 #!/user/bin/python
 import math
-count=0
-for a in [1000-x for x in range(1000)]:
-    for b in [x+2 for x in range(math.ceil(math.sqrt(a)))]:
-        if a/b == int(a/b):
+while 1==1:
+    count=0
+    n=int(input("请输入你希望倒序查找的素数开始"))
+    for a in [n-x for x in range(n)]:
+        for b in [x+2 for x in range(math.ceil(math.sqrt(a)))]:
+            if a/b == int(a/b):
+                break
+            if b==math.ceil(math.sqrt(a)):
+                print (a)
+                count=count+1
+        if count == 10:
             break
-        if b==math.ceil(math.sqrt(a)):
-            print (a)
-            count=count+1
-    if count == 10:
-        break
--- simple.py
+++ simple.py
@@ -2,16 +2,16 @@
 import math
 import time
 def my_try (answer):
-    for c in [x+1 for x in range(1000)]:
-        for b in [x+1 for x in range(1000)]:
-            for a in[x for x in range(1001)]:
+    for c in [x+1 for x in range(100)]:
+        for b in [x+1 for x in range(100)]:
+            for a in[x for x in range(101)]:
                 result = (b*math.sqrt(a))/c
-                if abs(result-abs(answer))<0.0000001:
+                if abs(result-abs(answer))<0.000000001:
                     print ('(',b,'*','sqrt',a,')','/',c)
                     return
 while 1==1:
-    d=float(input())
-    now1 = int(time.time())
+    d=float(input("请输入小数："))
+    now1 = time.time()
     my_try(d)
-    now2 = int(time.time())
-    print (now2-now1)
+    now2 = time.time()
+    print ("本次计算大约用时",now2-now1,"秒")
